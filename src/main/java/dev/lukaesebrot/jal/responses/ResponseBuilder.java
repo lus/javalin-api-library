2bb4311e4d2c69b5f40eee861376700c0fdf9194
Lukas Schulte Pelkum <lukaesebrot@gmail.com>
2020-02-15 15:11:59 +0100

    Add ResponseBuilder

---
+++ src/main/java/dev/lukaesebrot/jal/responses/ResponseBuilder.java
@@ -0,0 +1,67 @@
+package dev.lukaesebrot.jal.responses;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.google.gson.reflect.TypeToken;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+/**
+ * This class is used to build a json response
+ * @author Lukas Schulte Pelkum
+ * @version 1.0.0
+ * @since 1.0.0
+ */
+public class ResponseBuilder {
+
+    private int statusCode;
+    private ResponseType type;
+    private Map<String, Object> data;
+
+    /**
+     * Creates a new response builder
+     * @param statusCode The status code of the response
+     */
+    public ResponseBuilder(int statusCode) {
+        this.statusCode = statusCode;
+        this.type = ResponseType.SUCCESS;
+        this.data = new LinkedHashMap<>();
+    }
+
+    /**
+     * Defines the response type
+     * @param type The response type
+     * @return The new ResponseBuilder state
+     */
+    public ResponseBuilder withResponseType(ResponseType type) {
+        this.type = type;
+        return this;
+    }
+
+    /**
+     * Adds a new entry to the data map
+     * @param key The data key
+     * @param value The data itself
+     * @return The new ResponseBuilder state
+     */
+    public ResponseBuilder addData(String key, Object value) {
+        this.data.put(key, value);
+        return this;
+    }
+
+    /**
+     * Parses the current response state to a json string
+     * @return The parsed json string
+     */
+    public String toJson() {
+        JsonObject object = new JsonObject();
+
+        object.addProperty("status", statusCode);
+        object.addProperty("type", type.toString().toLowerCase());
+        object.add("data", new Gson().toJsonTree(data, new TypeToken<Map<String, Object>>(){}.getType()));
+
+        return object.toString();
+    }
+
+}
---
+++ src/main/java/dev/lukaesebrot/jal/responses/ResponseType.java
@@ -0,0 +1,12 @@
+package dev.lukaesebrot.jal.responses;
+
+/**
+ * This enum is used to specify the response types
+ * @author Lukas Schulte Pelkum
+ * @version 1.0.0
+ * @since 1.0.0
+ */
+public enum ResponseType {
+    ERROR,
+    SUCCESS
+}
