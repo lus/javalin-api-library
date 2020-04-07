251147c8c66cccf1792518b21b45df80a686df77
Cerus <zocks1703@web.de>
2020-04-07 17:53:03 +0200

    Make changes to the response builder and bump version by one minor patch

--- src/main/java/dev/lukaesebrot/jal/responses/ResponseBuilder.java
+++ src/main/java/dev/lukaesebrot/jal/responses/ResponseBuilder.java
@@ -2,6 +2,7 @@ package dev.lukaesebrot.jal.responses;
 
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
+import io.javalin.http.Context;
 import org.eclipse.jetty.http.HttpStatus;
 
 import java.util.LinkedHashMap;
@@ -9,8 +10,9 @@ import java.util.Map;
 
 /**
  * This class is used to build a json response
+ *
  * @author Lukas Schulte Pelkum
- * @version 1.0.0
+ * @version 1.0.1
  * @since 1.0.0
  */
 public class ResponseBuilder {
@@ -22,6 +24,7 @@ public class ResponseBuilder {
 
     /**
      * Creates a new response builder
+     *
      * @param statusCode The status code of the response
      */
     public ResponseBuilder(int statusCode) {
@@ -32,6 +35,7 @@ public class ResponseBuilder {
 
     /**
      * Defines the response type
+     *
      * @param type The response type
      * @return The new ResponseBuilder state
      */
@@ -42,7 +46,8 @@ public class ResponseBuilder {
 
     /**
      * Adds a new entry to the data map
-     * @param key The data key
+     *
+     * @param key   The data key
      * @param value The data itself
      * @return The new ResponseBuilder state
      */
@@ -53,6 +58,7 @@ public class ResponseBuilder {
 
     /**
      * Defines a raw entity to use for the data field
+     *
      * @param entity The object to use
      * @return The new ResponseBuilder state
      */
@@ -63,6 +69,7 @@ public class ResponseBuilder {
 
     /**
      * Parses the current response state to a json string
+     *
      * @return The parsed json string
      */
     public String toJson() {
@@ -75,20 +82,32 @@ public class ResponseBuilder {
         return object.toString();
     }
 
+    /**
+     * Sends the set status code and the set response to the context.
+     *
+     * @param ctx The context to respond to
+     */
+    public void respondTo(Context ctx) {
+        ctx.status(statusCode).result(toJson());
+    }
+
     public static String ok() {
         return new ResponseBuilder(HttpStatus.OK_200)
                 .withResponseType(ResponseType.SUCCESS)
                 .toJson();
     }
+
     public static String error(int errorCode, String errorMessage) {
         return new ResponseBuilder(errorCode)
                 .withResponseType(ResponseType.ERROR)
                 .addData("message", errorMessage)
                 .toJson();
     }
+
     public static String tooManyRequests() {
         return error(HttpStatus.TOO_MANY_REQUESTS_429, "You are being rate limited!");
     }
+
     public static String internalServerError(String errorMessage) {
         return error(HttpStatus.INTERNAL_SERVER_ERROR_500, errorMessage);
     }
